9ded0e82f960fd98ca6ef1f1cbb04f8f48df5608
SahilDhawan <sahildhawan2011@gmail.com>
2017-04-29 18:52:41 +0530

    cannot fetch data from NSFetchedResultsController

--- VirtualTourist/MapPinViewController.swift
+++ VirtualTourist/MapPinViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 import MapKit
 import CoreData
 
-class MapPinViewController: UIViewController {
+class MapPinViewController: UIViewController,NSFetchedResultsControllerDelegate {
     
     let delegate = UIApplication.shared.delegate as! AppDelegate
     
-    let photoSet = NSSet()
     var pin : MapPin? = nil
     
     @IBOutlet weak var mapView: MKMapView!
@@ -23,9 +22,15 @@ class MapPinViewController: UIViewController {
     
     @IBOutlet weak var noImageLabel: UILabel!
     
-    var photoArray : [[String:AnyObject]] = [[:]]
+    
+    var deletionIndexes : [IndexPath] = []
+    
+    var fetchedResultsController : NSFetchedResultsController<NSFetchRequestResult>? = nil
     
     override func viewWillAppear(_ animated: Bool) {
+        
+        self.noImageLabel.isHidden = true
+        
         //MapRegion
         let coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees((pin?.latitude)!), longitude: CLLocationDegrees((pin?.longitude)!))
         let region = MKCoordinateRegionMakeWithDistance(coordinate, 100, 100)
@@ -35,8 +40,6 @@ class MapPinViewController: UIViewController {
         
         //No User Interaction
         self.mapView.isUserInteractionEnabled = false
-        
-        flickrRequest()
     }
     
     func flickrRequest()
@@ -53,7 +56,24 @@ class MapPinViewController: UIViewController {
                         {
                             let dataDict = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! NSDictionary
                             let photoDict = dataDict["photos"] as! [String:AnyObject]
-                            self.photoArray = photoDict["photo"] as! [[String : AnyObject]]
+                            let photoArray = photoDict["photo"] as! [[String : AnyObject]]
+                            for i in 0..<photoArray.count
+                            {
+                                let dict = photoArray[i]
+                                let imageString : String? = dict["url_m"] as? String
+                                if let string = imageString
+                                {
+                                    let imageUrl = URL(string:string)
+                                    do
+                                    {
+                                        if let imageData = try? Data(contentsOf: imageUrl!)
+                                        {
+                                            let _ = PhotoAlbum(imageData as NSData,self.delegate.persistentContainer.viewContext)
+                                            self.delegate.saveContext()
+                                        }
+                                    }
+                                }
+                            }
                             DispatchQueue.main.async {
                                 self.collectionView.reloadData()
                             }
@@ -73,6 +93,8 @@ class MapPinViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //collectionView
+        collectionView.delegate = self
         collectionView.dataSource = self
         
         //dimension for collectionViewCell
@@ -81,6 +103,24 @@ class MapPinViewController: UIViewController {
         collectionFlowLayout.minimumLineSpacing = space
         collectionFlowLayout.minimumInteritemSpacing = space
         collectionFlowLayout.itemSize = CGSize(width: dimension, height: dimension)
+        
+        //NSFetchedResultsController
+        self.fetchedResultsController?.delegate = self
+        
+        let fetchRequest:NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName : "PhotoAlbum")
+        fetchRequest.predicate = NSPredicate(format: "location == %@", argumentArray: [pin!])
+        let sortArray = [NSSortDescriptor]()
+        fetchRequest.sortDescriptors = sortArray
+        
+        self.fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest , managedObjectContext: delegate.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        do
+        {
+            try  fetchedResultsController?.performFetch()
+        }
+        catch
+        {
+            Alert().showAlert(self, "Cannot perform fetch")
+        }
     }
     
     @IBAction func newCollectionPressed(_ sender: Any) {
@@ -92,7 +132,6 @@ class MapPinViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.noImageLabel.isHidden = true
         self.delegate.saveContext()
     }
     
@@ -106,65 +145,36 @@ extension MapPinViewController : UICollectionViewDataSource
         cell.indicatorView.startAnimating()
         
         DispatchQueue.global(qos: .userInitiated).async {
-          
-                let photoDict = self.photoArray[indexPath.item]
-                let imageString : String? = photoDict["url_m"] as? String
-                if let string = imageString
-                {
-                    let imageUrl = URL(string:string)
-                    do
-                    {
-                        if let imageData = try? Data(contentsOf: imageUrl!)
-                        {
-                            DispatchQueue.main.async {
-                                
-                                let image = UIImage(data: imageData)
-                                
-                                //Saving data to CoreData
-                                let photo = PhotoAlbum(imageData as NSData,self.delegate.persistentContainer.viewContext)
-                                self.photoSet.adding(photo)
-                                self.pin?.addToPhoto(self.photoSet)
-                                
-                                cell.imageView.image = image
-                                cell.imageView.alpha = 1.0
-                                cell.indicatorView.stopAnimating()
-                            }
-                        }
-                        else
-                        {
-                            Alert().showAlert(self, "No image exists at this url")
-                        }
-                    }
-                }
-        }
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let dictSize = photoArray.count
-        var returnSize : Int = 0
-        if dictSize >= 21
-        {
-            self.noImageLabel.isHidden = true
-            returnSize = 21
-        }
-        else if dictSize < 21
-        {
-            returnSize = dictSize
-            
-            if dictSize == 0
+           //To check if the pin already contains a set of photos
+            if (self.fetchedResultsController?.fetchedObjects) != nil
             {
-                self.noImageLabel.isHidden = false
+                let imageData = self.fetchedResultsController?.object(at: indexPath) as! PhotoAlbum
+                DispatchQueue.main.async {
+                    let image = UIImage(data: imageData.image as! Data)
+                    
+                    cell.imageView.image = image
+                    cell.imageView.alpha = 1.0
+                    cell.indicatorView.stopAnimating()
+                }
             }
             else
             {
-                self.noImageLabel.isHidden = true
+                self.flickrRequest()
             }
         }
-        return returnSize
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (fetchedResultsController?.sections?.count)!
     }
 }
-extension MapPinViewController : NSFetchedResultsControllerDelegate
+extension MapPinViewController : UICollectionViewDelegate
 {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "flickrCell", for: indexPath) as! CollectionViewCell
+        cell.imageView.alpha = 0.2
+        self.deletionIndexes.append(indexPath)
+    }
 }
+
+
